b9c2ee0e33a08b40d879d8ccf0a3593dbc05b054
SergeyTyurin <bwert@yandex.ru>
2023-02-14 17:06:50 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,63 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"errors"
+	"regexp"
+	"sort"
+	"strings"
+	"unicode"
+)
+
+var (
+	errorValidString = errors.New("string contains more 1 words")
+	N                = 10
+)
+
+func split(str string) []string {
+	f := func(c rune) bool {
+		return unicode.IsMark(c) || unicode.IsSpace(c)
+	}
+	return strings.FieldsFunc(str, f)
+}
+
+func getFormattedWord(str string) (string, error) {
+	if len(split(str)) > 1 {
+		return "", errorValidString
+	}
+	str = strings.ToLower(str)
+	matcher := `[^\p{P}]+(\p{P}*[^\p{P}]+)*`
+
+	validID := regexp.MustCompile(matcher)
+	formatted := validID.FindString(str)
+	return formatted, nil
+}
+
+func Top10(input string) []string {
+	words := split(input)
+	counter := make(map[string]int)
+	for _, word := range words {
+		word, _ = getFormattedWord(word)
+		if len(word) == 0 {
+			continue
+		}
+		counter[word]++
+	}
+
+	keys := make([]string, 0, len(counter))
+	for k := range counter {
+		keys = append(keys, k)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		if counter[keys[i]] == counter[keys[j]] {
+			return keys[i] < keys[j]
+		}
+		return counter[keys[i]] > counter[keys[j]]
+	})
+
+	if len(keys) > N {
+		keys = keys[:N]
+	}
+
+	return keys
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -79,4 +79,78 @@ func TestTop10(t *testing.T) {
 			require.Equal(t, expected, Top10(text))
 		}
 	})
+}
+
+func TestFormatWord(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "Текст", expected: "текст"},
+		{input: "!Текст", expected: "текст"},
+		{input: "Текст,", expected: "текст"},
+		{input: "тЕкст", expected: "текст"},
+		{input: "те,кСт", expected: "те,кст"},
+		{input: `'текст'`, expected: "текст"},
+		{input: `"текст"`, expected: "текст"},
+		{input: `"те8_-.кст"`, expected: "те8_-.кст"},
+		{input: ``, expected: ""},
+		{input: `и`, expected: "и"},
+		{input: `9`, expected: "9"},
+		{input: `94!`, expected: "94"},
+		{input: `,`, expected: ""},
+		{input: `!,`, expected: ""},
+		{input: `aaa.bbb.ccc`, expected: "aaa.bbb.ccc"},
+		{input: ",a", expected: "a"},
+		{input: ",a!", expected: "a"},
+		{input: "c.", expected: "c"},
+		{input: "a.a", expected: "a.a"},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.input, func(t *testing.T) {
+			result, err := getFormattedWord(test.input)
+			require.NoError(t, err)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func TestSplit(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []string
+	}{
+		{input: "Слово1 слово2", expected: []string{"Слово1", "слово2"}},
+		{input: "\nСлово1\nслово2\n", expected: []string{"Слово1", "слово2"}},
+		{input: "\nСлово1\t\tслово2\n", expected: []string{"Слово1", "слово2"}},
+		{input: "\nСлово1.слово2\n", expected: []string{"Слово1.слово2"}},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.input, func(t *testing.T) {
+			result := split(test.input)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func TestFormatWordError(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "Слово1 слово2", expected: ""},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.input, func(t *testing.T) {
+			result, err := getFormattedWord(test.input)
+			require.Error(t, err)
+			require.Equal(t, test.expected, result)
+		})
+	}
 }
