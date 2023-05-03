ed460382eb33027844792af2658da115b77c2054
SergeyTyurin <bwert@yandex.ru>
2023-05-03 13:17:09 +0300

    HW8 is completed (#8)

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,101 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("test without env", func(t *testing.T) {
+		file, _ := os.Create("testdata/test.sh")
+		defer os.Remove("testdata/test.sh")
+
+		text := `#!/usr/bin/env bash
+		echo test script`
+		file.WriteString(text)
+
+		code := RunCmd([]string{"sh", "testdata/test.sh"}, nil)
+		require.Equal(t, 0, code)
+	})
+
+	t.Run("test with non zero code", func(t *testing.T) {
+		file, _ := os.Create("testdata/test.sh")
+		defer os.Remove("testdata/test.sh")
+
+		text := `#!/usr/bin/env bash
+exit 1`
+		file.WriteString(text)
+
+		code := RunCmd([]string{"sh", "testdata/test.sh"}, nil)
+		require.Equal(t, 1, code)
+	})
+
+	t.Run("test without arguments", func(t *testing.T) {
+		file, _ := os.Create("testdata/test.sh")
+		defer os.Remove("testdata/test.sh")
+
+		text := `#!/usr/bin/env bash
+exit 1`
+		file.WriteString(text)
+
+		code := RunCmd([]string{"sh"}, nil)
+		require.Equal(t, 0, code)
+	})
+}
+
+func TestSetEnvironments(t *testing.T) {
+	t.Run("test set evnironment variable", func(t *testing.T) {
+		environment := make(Environment)
+		environment["TEST"] = EnvValue{Value: "Test value", NeedRemove: false}
+
+		script, _ := os.Create("testdata/script.sh")
+		text := `#!/usr/bin/env bash
+
+echo -n ${TEST}`
+
+		script.WriteString(text)
+
+		out := os.Stdout
+		test, _ := os.Create("testdata/testOut")
+		os.Stdout = test
+		defer func() {
+			os.Stdout = out
+			os.Remove("testdata/testOut")
+			os.Remove("testdata/script.sh")
+		}()
+
+		code := RunCmd([]string{"sh", "testdata/script.sh"}, environment)
+		content, _ := os.ReadFile(os.Stdout.Name())
+		require.Equal(t, 0, code)
+		require.Equal(t, "Test value", string(content))
+	})
+
+	t.Run("test unset evnironment variable", func(t *testing.T) {
+		os.Setenv("TEST", "Test value")
+		environment := make(Environment)
+		environment["TEST"] = EnvValue{Value: "New Value", NeedRemove: true}
+
+		script, _ := os.Create("testdata/script.sh")
+		text := `#!/usr/bin/env bash
+
+echo -n ${TEST}`
+
+		script.WriteString(text)
+
+		out := os.Stdout
+		test, _ := os.Create("testdata/testOut")
+		os.Stdout = test
+		defer func() {
+			os.Stdout = out
+			os.Remove("testdata/testOut")
+			os.Remove("testdata/script.sh")
+		}()
+
+		code := RunCmd([]string{"sh", "testdata/script.sh"}, environment)
+		content, _ := os.ReadFile(os.Stdout.Name())
+		require.Equal(t, 0, code)
+		require.Equal(t, "", string(content))
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
+var (
+	argErrormsg = `not enough command line arguments.
+	(Should be "./go-env path/to/env/ command arg1 arg2 ...")`
+
+	errorEnvDirCode = 111
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatal(argErrormsg)
+	}
+	dir := os.Args[1]
+	environment, err := ReadDir(dir)
+	if err != nil {
+		os.Exit(errorEnvDirCode)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], environment))
 }
