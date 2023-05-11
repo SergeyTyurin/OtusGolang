dc0f8967a2938a3b1c2cf55113af000154df6cc0
SergeyTyurin <bwert@yandex.ru>
2023-05-11 09:21:00 +0300

    HW9 is completed (#9)

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,14 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/SergeyTyurin/OtusGolang/hw09_struct_validator/internal/rules"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +17,103 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var builder strings.Builder
+	builder.WriteString("Validation Error:\n")
+	for _, e := range v {
+		builder.WriteString("\tField: " + e.Field + ": " + e.Err.Error() + "\n")
+	}
+	return builder.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	if v == nil {
+		return fmt.Errorf("interface is nil")
+	}
+
+	t := reflect.TypeOf(v)
+	if t.Kind() != reflect.Struct {
+		return fmt.Errorf("input isn not struct")
+	}
+
+	return validateStruct(v)
+}
+
+func validateStruct(s interface{}) error {
+	v := reflect.ValueOf(s)
+	t := reflect.TypeOf(s)
+	validationErrors := make(ValidationErrors, 0)
+
+	for i := 0; i < v.NumField(); i++ {
+		if !v.Field(i).CanInterface() {
+			continue
+		}
+		var ruleValidationError rules.ValidationError
+		err := validateField(t.Field(i), v.Field(i).Interface())
+		if err != nil {
+			if errors.As(err, &ruleValidationError) {
+				vError := ValidationError{Field: t.Field(i).Name, Err: err}
+				validationErrors = append(validationErrors, vError)
+			} else {
+				return err
+			}
+		}
+	}
+	if len(validationErrors) != 0 {
+		return validationErrors
+	}
+
+	return nil
+}
+
+func validateField(field reflect.StructField, value interface{}) error {
+	fieldType := field.Type.Kind()
+	if fieldType == reflect.Slice {
+		fieldType = field.Type.Elem().Kind()
+	}
+	if fieldType != reflect.Int && fieldType != reflect.String {
+		return nil
+	}
+
+	strTag, ok := field.Tag.Lookup("validate")
+	if !ok {
+		return nil
+	}
+
+	if field.Type.Kind() == reflect.Slice {
+		reflectedSlice := reflect.ValueOf(value)
+		for i := 0; i < reflectedSlice.Len(); i++ {
+			err := checkValue(strTag, reflectedSlice.Index(i).Interface())
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	return checkValue(strTag, value)
+}
+
+func checkValue(tag string, value interface{}) error {
+	fieldType := reflect.ValueOf(value).Type().Kind()
+	rulesStr := strings.Split(tag, "|")
+	for _, ruleStr := range rulesStr {
+		pair := strings.Split(ruleStr, ":")
+		if len(pair) != 2 {
+			return fmt.Errorf("incorrect format for tag %s", tag)
+		}
+		rule, err := rules.GetRule(pair[0], fieldType)
+		if err != nil {
+			typeError := rules.TypeError{}
+			if errors.As(err, &typeError) {
+				continue
+			} else {
+				return err
+			}
+		}
+
+		err = rule.CheckValue(pair[1], value)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,12 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -36,25 +40,86 @@ type (
 	}
 )
 
+func jsonBlob(filename string) []byte {
+	blob, _ := os.ReadFile(filename)
+	return blob
+}
+
 func TestValidate(t *testing.T) {
+	var correctUser User
+	var correctApp App
+	var correctResponse Response
+	token := Token{}
+	var incorrectUser User
+	var incorrectApp App
+	var incorrectResponse Response
+
+	json.Unmarshal(jsonBlob("testdata/correct/user.json"), &correctUser)
+	json.Unmarshal(jsonBlob("testdata/correct/app.json"), &correctApp)
+	json.Unmarshal(jsonBlob("testdata/correct/response.json"), &correctResponse)
+	json.Unmarshal(jsonBlob("testdata/wrong/user.json"), &incorrectUser)
+	json.Unmarshal(jsonBlob("testdata/wrong/app.json"), &incorrectApp)
+	json.Unmarshal(jsonBlob("testdata/wrong/response.json"), &incorrectResponse)
+
 	tests := []struct {
 		in          interface{}
 		expectedErr error
 	}{
-		{
-			// Place your code here.
-		},
-		// ...
-		// Place your code here.
+		{in: correctUser, expectedErr: nil},
+		{in: correctApp, expectedErr: nil},
+		{in: correctResponse, expectedErr: nil},
+		{in: token, expectedErr: nil},
+		{in: incorrectUser, expectedErr: ValidationErrors{}},
+		{in: incorrectApp, expectedErr: ValidationErrors{}},
+		{in: incorrectResponse, expectedErr: ValidationErrors{}},
 	}
 
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
 			tt := tt
 			t.Parallel()
-
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			if tt.expectedErr != nil {
+				require.ErrorAs(t, err, &tt.expectedErr)
+			} else {
+				require.Nil(t, err)
+			}
 		})
 	}
+}
+
+func TestProgramErrors(t *testing.T) {
+	t.Run("input case", func(t *testing.T) {
+		require.NotNil(t, Validate(nil))
+		require.NotNil(t, Validate(""))
+	})
+
+	tests := []struct {
+		in interface{}
+	}{
+		{in: struct {
+			Field int `validate:"in200,404,500"`
+		}{Field: 1}},
+		{in: struct {
+			Field int `validate:"min:a"`
+		}{Field: 1}},
+		{in: struct {
+			Field int `validate:"len:5"`
+		}{Field: 1}},
+		{in: struct {
+			Field string `validate:"len:"`
+		}{Field: "1"}},
+		{in: struct {
+			Field string `validate:"length:1"`
+		}{Field: "1"}},
+	}
+
+	t.Run("parsing case", func(t *testing.T) {
+		var validationErrors ValidationErrors
+		for _, test := range tests {
+			err := Validate(test.in)
+			require.NotNil(t, err)
+			require.False(t, errors.As(err, &validationErrors))
+		}
+	})
 }
