f3da0a206ecf6018ed7bf8d25dadfec74bc83f15
SergeyTyurin <bwert@yandex.ru>
2023-05-31 11:19:47 +0300

    HW11 is comleted

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,77 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var (
+	tmpWithTime    = "go-telnet --timeout=10s host port"
+	tmpWithoutTime = "go-telnet host port"
+	errFormatStr   = `args error: require "%v" or "%v"`
+	timeout        time.Duration
+)
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "file to read from")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+	withTimeout := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "timeout" {
+			withTimeout = true
+		}
+	})
+	addr, err := parseCommandLine(os.Args, withTimeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := NewTelnetClient(addr, timeout, os.Stdin, os.Stdout)
+	err = client.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	fmt.Fprintln(os.Stderr, "...connected to", addr)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	go func() {
+		defer cancel()
+		err = client.Send()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		defer cancel()
+		err = client.Receive()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	<-ctx.Done()
+}
+
+func parseCommandLine(args []string, withTimeout bool) (string, error) {
+	count := len(args)
+	if (withTimeout && count != 4) || (!withTimeout && count != 3) {
+		return "", fmt.Errorf(errFormatStr, tmpWithTime, tmpWithoutTime)
+	}
+
+	host := args[count-2]
+	port := args[count-1]
+
+	return net.JoinHostPort(host, port), nil
 }
